210b117ca0f64481e54329d8ec0185e9257a1c8b
KAUSHALJHA12 <118800368+KAUSHALJHA12@users.noreply.github.com>
2024-11-20 19:19:10 +0000

    datatype

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const accountID = 14453
+let accountEmail = "jhakaushal0017@google.com"
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212221"
+acoountCity = "Pune"
+console.table([accountEmail, accountPassword, accountID, acoountCity])
